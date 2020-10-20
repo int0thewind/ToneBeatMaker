2373a44463188bdc33c10f94f47b893ec73aa96a
Hanzhi Yin <yinhanzhi@gmail.com>
2020-10-20 13:38:07 -0500

    optimized css style

--- src/BeatMaker.js
+++ src/BeatMaker.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Button, ButtonGroup } from '@material-ui/core';
+import { makeStyles, Button, ButtonGroup, Paper, Card } from '@material-ui/core';
 import { red, orange, yellow, lime, green, blue, purple, brown } from '@material-ui/core/colors'
 import * as Tone from 'tone';
 
@@ -10,14 +10,7 @@ const ANIMATION_DURATION = '0.15s';
 const MARGIN = '4pt 4pt 4pt 4pt';
 
 const beatMakerContainerStyle = makeStyles((theme) => ({
-  beatMakerBoard: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    margin: MARGIN
-  },
-  beatMakerContainer: {
+  beatMakerPaper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -29,9 +22,12 @@ const beatMakerContainerStyle = makeStyles((theme) => ({
     width: '90vw',
     position: "fixed",
     bottom: 0,
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: theme.shape.borderRadius,
-    boxShadow: theme.shadows[3]
+  },
+  beatMakerBoard: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: MARGIN
   },
   beatMakerButtonGroup: {
     margin: MARGIN
@@ -51,7 +47,7 @@ function BeatMakerContainer() {
   }
 
   return (
-    <div className={classes.beatMakerContainer}>
+    <Paper className={classes.beatMakerPaper} elevation='4'>
       <div className={classes.beatMakerBoard}>
         <BeatMakerCard keyCode='1' tone='C4' toDisplay={1 <= cardCount} color={red}/>
         <BeatMakerCard keyCode='2' tone='D4' toDisplay={2 <= cardCount} color={orange}/>
@@ -62,31 +58,28 @@ function BeatMakerContainer() {
         <BeatMakerCard keyCode='7' tone='B4' toDisplay={7 <= cardCount} color={purple}/>
         <BeatMakerCard keyCode='8' tone='C5' toDisplay={8 <= cardCount} color={brown}/>
       </div>
-      <div>
       <ButtonGroup size='medium' variant='contained' color='primary' className={classes.beatMakerButtonGroup}>
         <Button onClick={incCount} disabled={cardCount === 8}> + </Button>
         <Button onClick={decCount} disabled={cardCount === 1}> - </Button>
       </ButtonGroup>
-      </div>
-    </div>
+    </Paper>
   )
 }
 
 const beatMakerCardStyle = makeStyles((theme) => ({
   beatMakerCard: {
-    width: props => props.toDisplay ? '84pt' : '0pt',
     height: '40pt',
-    margin: MARGIN,
     borderRadius: '8pt',
+    width: props => props.toDisplay ? '84pt' : '0pt',
+    margin: props => props.toDisplay ? MARGIN : '0pt 0pt 0pt 0pt',
     backgroundColor: props => props.color[500],
     '&:hover': {
       backgroundColor: props => props.color[200]
     },
     opacity: props => props.toDisplay ? '1' : '0',
     transition: `opacity ${ANIMATION_DURATION} ease-in-out,
-      width ${ANIMATION_DURATION} ease-out,
-      background-color ${ANIMATION_DURATION} ease-in-out`,
-      boxShadow: theme.shadows[1]
+      width ${ANIMATION_DURATION} ease-in-out,
+      background-color ${ANIMATION_DURATION} ease-out`,
   }
 }))
 
@@ -99,10 +92,11 @@ function BeatMakerCard(props) {
   }
 
   return (
-    <div
+    <Card
+      raised={true}
       className={`${classes.beatMakerCard} beat-maker-card`}
       onClick={mouseClickPlay}>
-    </div>
+    </Card>
   )
 }
 
